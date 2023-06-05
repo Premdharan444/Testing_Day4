c41deac150c101f318bb2a9e568e3adf2387f6f6
Premdharan444 <135309067+Premdharan444@users.noreply.github.com>
2023-06-05 20:43:40 +0530

    Add files via upload

---
+++ Day4/src/main/java/selenium/Day4/App.java
@@ -0,0 +1,13 @@
+package selenium.Day4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
